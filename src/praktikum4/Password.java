ed20119109431bb9218b73aaad005fe84c6af631
hekoppel <hekoppel@klass1-14-l.intra.itcollege.ee>
2016-09-09 11:26:01 +0300

    Adding third task to praktikum4

---
+++ src/praktikum4/Password.java
@@ -0,0 +1,24 @@
+package praktikum4;
+
+import lib.TextIO;
+
+public class Password {
+
+	public static void main(String[] args) {
+		String salasona;
+		
+		System.out.println("Palun sisesta oma salasõna. ");
+		salasona = TextIO.getln();
+		
+		if (salasona.equals("saladus"))
+			System.out.println("Õige salasõna ");
+		else 
+			System.out.println("Vale salasõna ");
+
+		
+		
+		
+
+	}
+
+}
