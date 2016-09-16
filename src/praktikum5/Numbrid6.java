ec41e53332fc82fd44fb8a6d80947fc87267bfde
Heidi Koppel <hekoppel@klass1-06-l.intra.itcollege.ee>
2016-09-16 13:31:17 +0300

    Adding Numbrid6

---
+++ src/praktikum5/Numbrid6.java
@@ -0,0 +1,18 @@
+package praktikum5;
+
+public class Numbrid6 {
+
+	public static void main(String[] args) {
+		int tabeliSuurus = 5;
+		
+		for (int i = 0 ; i < tabeliSuurus; i++) {
+			for (int j = 0; j < tabeliSuurus; j++) {
+				System.out.print((i+j + " "));
+				}
+		}
+		System.out.println();
+	}
+
+}
+
+
