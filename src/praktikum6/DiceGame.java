8f63cab9d437a6b96a3c562c481d4e3967ae5925
Heidi Koppel <heidi.koppel@itcollege.ee>
2016-09-23 13:42:41 +0300

    Adding DiceGame

---
+++ src/praktikum6/DiceGame.java
@@ -0,0 +1,38 @@
+package praktikum6;
+
+import java.util.Random;
+
+public class DiceGame {
+
+	// Kirjutada täringumäng:
+	// Programm viskab kaks täringut mängijale ja kaks täringut endale
+	// (arvutile), arvutab mõlema mängija summad ja teatab, kellel oli rohkem.
+
+	public static void main(String[] args) {
+		int Player1 = throwDice() + throwDice();
+		int Player2 = throwDice() + throwDice();
+
+		System.out.println("Mängija 1 summa on " + Player1);
+		System.out.println("Mängija 2 summa on " + Player2);
+
+		if (Player1 > Player2) {
+			System.out.println("Mängija 1 võitis");
+		} else if (Player1 < Player2) {
+			System.out.println("Mängija 2 võitis");
+		} else {
+			System.out.println("Viik!!");
+		}
+
+	}
+
+	public static int throwDice() {
+
+		Random rand = new Random();
+		// nextInt excludes the top value so we have to add 1 to include the top
+		// value
+		int randomNum = rand.nextInt(5 + 1) + 1;
+
+		return randomNum;
+	}
+
+}
