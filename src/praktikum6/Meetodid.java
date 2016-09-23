efe281df68a5449b3eec1c9a4df1a37cc9a806b7
Heidi Koppel <hekoppel@klass1-02-l.intra.itcollege.ee>
2016-09-23 10:55:24 +0300

    First two of praktikum6

---
+++ src/praktikum6/Meetodid.java
@@ -0,0 +1,23 @@
+package praktikum6;
+
+import lib.TextIO;
+
+public class Meetodid {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Sisesta üks täisarv");
+		
+		int arv = TextIO.getlnInt();
+		int arvKuubis = kuup(arv);
+		
+		System.out.println(arvKuubis);
+
+	}
+	
+	public static int kuup(int number ) {
+	
+		return number*number*number; 
+	}
+
+}
---
+++ src/praktikum6/Vahemik.java
@@ -0,0 +1,34 @@
+package praktikum6;
+
+import lib.TextIO;
+
+public class Vahemik {
+
+	public static void main(String[] args) {
+		
+		int number = kasutajaSisestus(1, 5);
+		System.out.println("Kasutaja sisestas " + number);
+		
+		
+
+	}
+	
+	public static int kasutajaSisestus(int min, int max) {
+		
+		System.out.println("Sisesta üks täisarv");
+		
+		int sisestus = TextIO.getlnInt();
+		
+		
+		while (min > sisestus || max < sisestus) {
+			
+			System.out.println("Ei sobi, sisesta uus täisarv");
+			sisestus = TextIO.getlnInt();
+		
+		} 
+		return sisestus;
+		
+		
+	}
+
+}
