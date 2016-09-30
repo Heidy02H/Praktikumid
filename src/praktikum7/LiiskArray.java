9a55b0f6ba3f2a71de1999e64815c340e6543b73
Heidi Koppel <heidi.koppel@itcollege.ee>
2016-09-30 13:03:53 +0300

    Adding LiiskArray - work in progress

---
+++ src/praktikum7/LiiskArray.java
@@ -0,0 +1,22 @@
+package praktikum7;
+
+import lib.TextIO;
+
+public class LiiskArray {
+
+	public static void main(String[] args) {
+
+		System.out.println("Sisesta inimeste arv");
+
+		int inimesteArv = TextIO.getlnInt();
+		
+		String[] nimekiri = new String[inimesteArv];
+		
+		System.out.println("Sisesta inimeste nimed");
+		
+		
+		
+
+	}
+
+}
