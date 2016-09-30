3219cd37fcec4117c10c0383bdd1a718fb0efab9
Heidi Koppel <hekoppel@klass1-08-l.intra.itcollege.ee>
2016-09-30 11:34:02 +0300

    Adding Sona.java

---
+++ src/praktikum7/Sona.java
@@ -0,0 +1,19 @@
+package praktikum7;
+
+import lib.TextIO;
+
+public class Sona {
+
+	public static void main(String[] args) {
+		System.out.println("Palun sisesta üks sõna ");
+		String sona = TextIO.getln();
+		
+		for (int i = sona.length() ; i >= 0 ; i--) {
+			
+		}
+		
+		
+
+	}
+
+}
