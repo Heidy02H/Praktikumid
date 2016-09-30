98c0e5a7fc330c6ada6c9d428ce4def9512d6ab3
Heidi Koppel <heidi.koppel@itcollege.ee>
2016-09-30 11:55:55 +0300

    Adding changes to Fox, RebaneJaTaring and Taring

---
+++ src/praktikum5/RebanejaTaring.java
@@ -0,0 +1,23 @@
+package praktikum5;
+
+public class RebanejaTaring {
+
+	public static void main(String[] args) {
+	
+		String fox[] = {
+				" /\\           ",
+				"(~(           ",
+				" ) )     /\\_/\\",
+				"( _-----_(@ @)",
+				"  (       \\ / ",
+				"  /|/--\\|\\ V  ",
+				"  \" \"   \" \"   "
+			};
+		Fox.main(fox);
+			
+			
+	}
+	
+	
+
+}
